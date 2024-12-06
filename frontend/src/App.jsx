374b3e6857aff93285fd9432a621015c39207c9c
Mabrook-Alizai <mabkalizai@gmail.com>
2024-12-06 10:43:33 +0500

    footer has been added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,23 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import PlaceOrder from './pages/placeOrder/placeOrder'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        < Route path='/' element={<Home/>} />
-        < Route path='/cart' element={<Cart/>} />
-        < Route path='/order' element={<PlaceOrder/>} />
-
-      </Routes>
-    </div>
+    <>
+     <div className='app'>
+       <Navbar/>
+       <Routes>
+         < Route path='/' element={<Home/>} />
+         < Route path='/cart' element={<Cart/>} />
+         < Route path='/order' element={<PlaceOrder/>} />
+ 
+       </Routes>
+     </div>
+     <Footer/>
+    </>
   )
 }
 
