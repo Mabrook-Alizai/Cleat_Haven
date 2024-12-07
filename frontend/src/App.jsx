9c6ccc1a316845ce85e3305e553770d095ca5370
Mabrook-Alizai <mabkalizai@gmail.com>
2024-12-07 15:24:28 +0500

    Cart and Order page has been added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/NavBar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import PlaceOrder from './pages/placeOrder/placeOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopup/LoginPopUp'
 
 
 const App = () => {
+
+const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/> : <></>}
      <div className='app'>
-       <Navbar/>
+       <Navbar setShowLogin={setShowLogin}/>
        <Routes>
          < Route path='/' element={<Home/>} />
          < Route path='/cart' element={<Cart/>} />
