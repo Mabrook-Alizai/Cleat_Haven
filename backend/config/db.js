a89b8c5aaf17e7e530ad1e7d4f777742cab57b4d
Mabrook-Alizai <mabkalizai@gmail.com>
2024-12-15 08:34:25 +0500

    Backend recreated due to issue

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://MabrookKhanAlizai:%23Mabrook1@cluster0.jqncg.mongodb.net/cleat-haven').then(()=>console.log("DB connected"));
-}
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,18 @@
 import express from "express"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
 
-
-
-// App config
+// App Config
 const app = express()
 const port = 4000
 
-// Middleware
+// middleware
 app.use(express.json())
 app.use(cors())
 
-// db connection
-connectDB();
-
 app.get("/", (req, res)=>{
-    res.send("API Working")
+    res.send("API working")
 })
 
 app.listen(port, ()=>{
-    console.log(`Server Started On http://localhost:${port}`)
+    console.log(`Server started at http://localhost:${port}`)
 })
-
