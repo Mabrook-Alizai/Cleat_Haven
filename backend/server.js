0c1a5c2e9adb6f549ef7e79029126e1df48afd1e
Mabrook-Alizai <mabkalizai@gmail.com>
2025-01-10 20:59:22 +0500

    updated backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express()
 const port = 4000
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 app.use(express.json())
 
 // DataBase Connection
